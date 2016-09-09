900233a309b0664892b5834ba55e620c581bf9f2
wxfeng713728 <584425631@qq.com>
2016-09-09 15:06:11 +0800

    Add files via upload
    python list pirnt util

---
+++ print_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+def print_list(the_list):
+    for each_item in the_list:
+        if isinstance(each_item,list) :
+            print_list(each_item)
+        else:
+            print(each_item)
